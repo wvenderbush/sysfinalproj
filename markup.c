884c456c8b854a11ba713743f0a3c9c266be6a65
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-10 20:05:22 -0500

    added to text stuff

--- markup.c
+++ markup.c
@@ -13,32 +13,111 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-char * parse(char *input){
+char * multitok(char *input, char *delimiter){
+    static char *string;
+    if (input != NULL)
+        string = input;
+
+    if (string == NULL)
+        return string;
+
+    char *end = strstr(string, delimiter);
+    if (end == NULL) {
+        char *temp = string;
+        string = NULL;
+        return temp;
+    }
+
+    char *temp = string;
+
+    *end = '\0';
+    string = end + strlen(delimiter);
+    return temp;
+}
+
+
+char * parse(char *input){ //multiple tags in one line (ie. 2 ::red:: breaks the code because of multitok)
 	if (strstr(input, "::red::") != NULL){
-		char *output = strcat(ANSI_COLOR_RED, input);
-		output = strcat(input, ANSI_COLOR_RESET);
+		char output[1024];
+		char *start = multitok(input, "::red::");
+		char *end = multitok(NULL, "::red::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_RED);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
 		return output;
 	}
 	else if (strstr(input, "::green::") != NULL){
-
+		char output[1024];
+		char *start = multitok(input, "::green::");
+		char *end = multitok(NULL, "::green::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_GREEN);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
+		return output;
 	}
 	else if (strstr(input, "::yellow::") != NULL){
-
+		char output[1024];
+		char *start = multitok(input, "::yellow::");
+		char *end = multitok(NULL, "::yellow::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_YELLOW);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
+		return output;
 	}
 	else if (strstr(input, "::blue::") != NULL){
-
+		char output[1024];
+		char *start = multitok(input, "::blue::");
+		char *end = multitok(NULL, "::blue::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_BLUE);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
+		return output;
 	}
 	else if (strstr(input, "::magenta::") != NULL){
+		char output[1024];
+		char *start = multitok(input, "::magenta::");
+		char *end = multitok(NULL, "::magenta::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_MAGENTA);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
+		return output;
 
 	}
 	else if (strstr(input, "::cyan::") != NULL){
+		char output[1024];
+		char *start = multitok(input, "::cyan::");
+		char *end = multitok(NULL, "::cyan::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_CYAN);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
+		return output;
 
 	}
+	else if (strstr(input, "::basic::") != NULL){
+		char output[1024];
+		char *start = multitok(input, "::basic::");
+		char *end = multitok(NULL, "::basic::");
+		strcpy(output, start);
+		strcat(output, ANSI_COLOR_RESET);
+		strcat(output, end);
+		strcat(output, ANSI_COLOR_RESET);
+		return output;
+	}
+	return input;
+}
 
-
-
-
-
+char * multiparse(char *input){
+	while (strstr(input, "::") != 0){ //this is a dangerous base case
+		char *new = parse(input);
+		strcpy(input, new);
+	}
+	return input;
 }
 
 int main(int argc, char const *argv[]) {
@@ -50,9 +129,9 @@ int main(int argc, char const *argv[]) {
   printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n\n\n");
 
-  char test[] = "::red::testing";
+  char test[] = "This text is ::red::red ::green::and ::magenta::this is ::cyan::blue";
 
-  printf(parse(test));
+  printf("%s\n", multiparse(test));
 
   return 0;
 }
