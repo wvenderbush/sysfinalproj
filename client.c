3b83a8f97c5e9f2d2155e3ef018d96eb2d6e5b6f
max Bertfield <mbertfield@stuy.edu>
2017-01-16 15:17:46 -0500

    its almost working

--- client.c
+++ client.c
@@ -7,30 +7,37 @@
 
 int main( int argc, char *argv[] ) {
 
-  char *host;
-  if (argc != 2 ) {
-    printf("host not specified, conneting to 127.0.0.1\n");
-    host = "127.0.0.1";
-  }
-  else
-    host = argv[1];
+  char host[100];
+  printf("enter a host to connect to (format xxx.xxx.xxx.xxx) >");
+  fgets(host, sizeof(host), stdin);
+  *strchr(host, '\n') = 0;
   
+  
+   
   int sd;
 
   sd = client_connect( host );
 
   char buffer[MESSAGE_BUFFER_SIZE];
-  
-  while (1) {
-    printf("enter message: ");
-    fgets( buffer, sizeof(buffer), stdin );
-    char *p = strchr(buffer, '\n');
-    *p = 0;
-  
-    write( sd, buffer, sizeof(buffer) );
-    read( sd, buffer, sizeof(buffer) );
-    printf( "received: %s\n", buffer );
+  int f = fork();
+  if (f == 0) {
+    printf("%d", f);
+      while (1) {
+	printf("enter message: V \n");
+	fgets( buffer, sizeof(buffer), stdin );
+	char *p = strchr(buffer, '\n');
+	write( sd, buffer, sizeof(buffer) );
+	
+      }
+  }else{
+    printf("%d", f);
+    while(1){
+      char buffer2[MESSAGE_BUFFER_SIZE];
+      read ( sd, buffer2, sizeof(buffer2) );
+      printf("ip sent this: %s\n", buffer2);
+    }
   }
+
   
   return 0;
 }
--- server.c
+++ server.c
@@ -6,49 +6,50 @@
 #include "networking.h"
 
 void process( char * s );
-void sub_server( int sd );
+void sub_server( int sd, int pipe[] );
 
 int main() {
 
   int sd, connection;
 
   sd = server_setup();
-    
+
+  int descriptors[2];
+  pipe(descriptors);
+
   while (1) {
-
+   
     connection = server_connect( sd );
-
     int f = fork();
     if ( f == 0 ) {
 
       close(sd);
-      sub_server( connection );
+      sub_server(connection, descriptors);
 
       exit(0);
     }
     else {
       close( connection );
+      
     }
   }
   return 0;
 }
 
 
-void sub_server( int sd ) {
-
-  char buffer[MESSAGE_BUFFER_SIZE];
-  while (read( sd, buffer, sizeof(buffer) )) {
-
-    printf("[SERVER %d] received: %s\n", getpid(), buffer );
-    process( buffer );
-    write( sd, buffer, sizeof(buffer));    
-  }
-  
-}
-void process( char * s ) {
-
-  while ( *s ) {
-    *s = (*s - 'a' + 13) % 26 + 'a';
-    s++;
+void sub_server( int sd, int pipe[] ) {
+  int f = fork();
+  if (f == 0){
+    char buffer[MESSAGE_BUFFER_SIZE];
+    while (read( sd, buffer, sizeof(buffer) )) {
+       write(pipe[1], buffer, sizeof(buffer));    
+       printf("sent %s\n", buffer);
+    }
+  }else{
+    char buffer2[MESSAGE_BUFFER_SIZE];
+    while(read(pipe[0], buffer2, sizeof(buffer2))){
+      printf("recvieed from others: %s\n", buffer2);
+      write(sd, buffer2, sizeof(buffer2));
+      }
   }
 }
