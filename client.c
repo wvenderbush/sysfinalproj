39987cac21b49e0a537d45e3e983978fc9a5060a
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-23 23:31:08 -0500

    some final bugfixes

--- client.c
+++ client.c
@@ -12,7 +12,8 @@
 
 
 #define HEADER "   ______   ________          __\n  / ____/  / ____/ /_  ____ _/ /_\n / /      / /   / __ \\/ __ `/ __/\n/ /___   / /___/ / / / /_/ / /_\n\\____/   \\____/_/ /_/\\__,_/\\__/\n\n"
-
+#define WELCOME "Welcome to CCHat!\nHere are some helpful CML commands to get you started!\n\n::red:: turns text red\n::blue:: turns text blue\n::magenta:: turns text magenta\n::cyan:: turns text cyan\n::green:: turns text green\n::yellow:: turns text yellow\n::white:: turns text white\n::black:: turns text black\n::basic:: turns text back to the terminal default color\n\n"
+#define WELCOME2 "~~smile~~ creates ʘ‿ʘ\n~~shrug~~ creates ¯\\_(ツ)_/¯\n~~scared~~ creates ⊙﹏⊙\n~~disapprove~~ creates ಠ_ಠ\n~~cute~~ creates (｡◕‿◕｡)\n~~excited~~ creates ヽ(*▽*)/\n~~devious~~ creates ಠ‿ಠ\n~~tableflip~~ creates (╯°□°）╯︵ ┻━┻\n~~cat~~ creates ^ᵒᴥᵒ^\n~~fight~~ creates ლ(｀ー´ლ)\n~~bear~~ creates ʕ•ᴥ•ʔ\n~~blob~~ creates ༼ つ ◕_◕ ༽つ\n~~cry~~ creates ಥ_ಥ\n~~love~~ creates (づ￣ ³￣)づ\n~~confused~~ creates (⊙_☉)\n~~fightme~~ creates (ง •̀_•́)ง\n~~hiding~~ creates |_・)\n~~sleep~~ creates (ᴗ˳ᴗ)\n\n(scroll up to view some useful CML commands)\n"
 
 int run = 1;
 static void sighandler(int signo){
@@ -80,7 +81,6 @@ void printClear(int height){
 int main( int argc, char *argv[] ) {
   int err;
   signal(SIGINT, sighandler);
-  printf("%d", getHeight());
   char host[100];
   char rawuser[100];
   printf("\e[1;1H\e[2J");
@@ -102,6 +102,8 @@ int main( int argc, char *argv[] ) {
   char buffer[MESSAGE_BUFFER_SIZE];
   char newbuff[MESSAGE_BUFFER_SIZE];
   int f = fork();
+  printf(WELCOME);
+  printf(WELCOME2);
   printf(HEADER);
   printf("%s\n", promptLine(getWidth()));
   printClear(getHeight());
@@ -123,10 +125,10 @@ int main( int argc, char *argv[] ) {
 	error_check( err, "writing to server");
       }
     }
-    printf("[client] received exit signal, sending exit message to server\n");
+    printf("\nYou have been disconnected!\n");
     char *exiter = "EXIT";
     write(sd,exiter,5);
-    printf("writer exiting...\n");
+    //printf("writer exiting...\n");
     
     exit(1);
   }
@@ -139,7 +141,7 @@ int main( int argc, char *argv[] ) {
 	break;
       printf("\033[1A\x1b[K%s%s\n", buffer2, promptLine(getWidth()));
     }
-    printf("reader exiting... \n");
+    //printf("reader exiting... \n");
     exit(1);
       
   }
